100371d4a91ded6490d93dc591429a7ec2ae0e10
junniior <carlosjunior@trii.com.br>
2014-11-25 00:27:53 -0200

    Crud - Etapa 2: Recuperando valores do banco

--- app/controllers/projetosController.php
+++ app/controllers/projetosController.php
@@ -1,22 +1,22 @@
 <?php 
 	class Projetos extends Controller {
-		public function home () {
+		public function home() {
 			$this->view('projetosHome');
 		}
-		public function novo () {
+		public function novo() {
 			$this->view('projetosNovo');
 		}
-		public function edit () {
+		public function edit() {
 			$this->view('projetosEdit');
 		}
 
 		//Link que executa INSERT no db
-		public function add () {
+		public function add() {
 
 			$projetoNome = $_POST['pjt_new_1'];
 			$projetoData = $_POST['pjt_new_2'];
 
-			$db = new Projetos_Model ();
+			$db = new Projetos_Model();
 			$db->insert('sis_pjt', array(
 				"pjt_nme" => "$projetoNome",
 				"pjt_data" => "$projetoData",
@@ -25,15 +25,31 @@
 			return header( "Location: ../projetos" );
 		}
 
-		public function listaCompleta () {
-			$db = new Projetos_Model ();
-			$a = ($db->read('sis_pjt'));
-			foreach ($a as $key) {
-				$a1 = $key['pjt_id'];
-				$a2 = $key['pjt_nme'];
-				$a3 = $key['pjt_data'];
+		public function listaProjetos( $string ) {
+			$db = new Projetos_Model();
+			$read = ($db->read('sis_pjt'));
+				
+			switch ($string) {
+				case 'todos':
+					foreach ($read as $key) {
+						echo 
+						  '<tr>
+							<td>'.$key['pjt_id'].'</td>
+							<td>'.$key['pjt_nme'].'</td>
+							<td>'.$key['pjt_data'].'</td>
+						  </tr>'
+						;
+					}
+					break;
+				case 'finalizados':
+					//echo '<td>'.$key['pjt_nme'].'</td>';
+					break;
+				case 'abertos':
+					//echo '<td>'.$key['pjt_data'].'</td>';
+					break;
+				default:
+					//echo '';
+					break;
 			}
-
 		}
-
 	}
--- app/views/projetosHome.php
+++ app/views/projetosHome.php
@@ -1,40 +1,33 @@
-<?php include('system/header.php');
-
-	$b = new Projetos ();
-	$b->listaCompleta();
-
-?>
+<?php include('system/header.php'); ?>
 
 <div style="margin-left:10px;">
-<h2>O que você deseja fazer?</h2>
-
-<a href="projetos/novo">Adicionar um Projeto</a> <br>
+<h2>Meus Projetos</h2>
+<br />
+<a href="projetos/novo">Adicionar um Projeto</a> <p></p>
 <a href="projetos/edit">Editar um Projeto</a> <br>
-
-<br>
-
-<p>Para voltar <a href="dashboard">clique aqui</a>.</p>
 
 <br><br>
 
-<div style="width:33%; border:2px solid #c3c3c3;">
-
-<table class="table">
-      <thead>
-        <tr>
-          <th>Id do Projeto</th>
-          <th>Nome do Projeto</th>
-          <th>Data de Criação</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="active">
-          <td><?php  ?></td>
-          <td><?php  ?></td>
-          <td><?php  ?></td>
-        </tr>
-      </tbody>
-    </table>
-
+	<div>
+		<table class="table">
+	      <thead>
+	        <tr>
+	          <th>Id do Projeto</th>
+	          <th>Nome do Projeto</th>
+	          <th>Data de Criação</th>
+	        </tr>
+	      </thead>
+	      <tbody>
+			<?php 
+				$b = new Projetos ();
+				$b->listaProjetos('todos');
+			?>
+	      </tbody>
+	    </table>
     </div>
- </div>
+
+<br>
+
+<p>Para voltar <a href="dashboard">clique aqui</a>.</p>    
+
+</div>
